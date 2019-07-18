722f2f4db58c55e8a62b83294acd77465a3dc09a
644601935 <644601935@qq.com>
2019-07-18 10:12:44 +0800

    增加通用函数库-post

---
+++ app/Http/Services/APIHelper.php
@@ -0,0 +1,31 @@
+<?php
+namespace App\Http\Services;
+ 
+class APIHelper
+{
+ 
+    public function post($body,$apiStr)
+    {
+       
+
+        $client = new \GuzzleHttp\Client(['base_uri' => 'https://www.shikongmall.com/mobile/user.php?act=do_promoters_reward_code']);
+        $res = $client->request('POST', $apiStr,
+            ['form_params' =>$body,
+        ]);
+        $data = $res->getBody()->getContents();
+        return $data;
+    }
+ 
+    public function get($apiStr,$header)
+    {
+        $client = new \GuzzleHttp\Client(['base_uri' => 'http://192.168.31.XX:xxx/api/']);
+        $res = $client->request('GET', $apiStr,['headers' => $header]);
+        $statusCode= $res->getStatusCode();
+ 
+        $header= $res->getHeader('content-type');
+ 
+        $data = $res->getBody();
+ 
+        return $data;
+    }
+}
