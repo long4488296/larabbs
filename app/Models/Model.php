f70b45a7303dfc8f66b98c383206e9fd57308f7a
644601935 <644601935@qq.com>
2019-07-20 11:43:50 +0800

    为所有模型添加 主键排序/单个列名模糊查询

--- app/Models/Model.php
+++ app/Models/Model.php
@@ -6,14 +6,78 @@ use Illuminate\Database\Eloquent\Model as EloquentModel;
 
 class Model extends EloquentModel
 {
-    public function scopeRecent($query)
+    
+    public function scopeRecentReplied($query)
     {
-        return $query->orderBy('id', 'desc');
+        // 当话题有新回复时，我们将编写逻辑来更新话题模型的 reply_count 属性，
+        // 此时会自动触发框架对数据模型 updated_at 时间戳的更新
+        return $query->orderBy($this->primaryKey, 'desc');
     }
 
-    public function scopeOrdered($query)
+    public function scopeRecent($query)
     {
-        return $query->orderBy('order', 'desc');
+        // 按照创建时间排序
+        return $query->orderBy($this->primaryKey, 'asc');
+    }
+       /**
+     * @param   \Illuminate\Database\Eloquent\Builder $query
+     * @param     $column
+     * @param     $value
+     * @param     $side
+     * @param     $isNotLike
+     * @param     $isAnd
+     * @return    \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeLike($query, $column, $value, $side = 'both', $isNotLike = false, $isAnd = true)
+    {
+        if(!$value){return $query;}
+        $operator = $isNotLike ? 'not like' : 'like';
+
+        $escape_like_str = function ($str) {
+            $like_escape_char = '!';
+
+            return str_replace([$like_escape_char, '%', '_'], [
+                $like_escape_char.$like_escape_char,
+                $like_escape_char.'%',
+                $like_escape_char.'_',
+            ], $str);
+        };
+
+        switch ($side) {
+            case 'none':
+                $value = $escape_like_str($value);
+                break;
+            case 'before':
+            case 'left':
+                $value = "%{$escape_like_str($value)}";
+                break;
+            case 'after':
+            case 'right':
+                $value = "{$escape_like_str($value)}%";
+                break;
+            case 'both':
+            case 'all':
+            default:
+                $value = "%{$escape_like_str($value)}%";
+                break;
+        }
+
+        return $isAnd ? $query->where($column, $operator, $value) : $query->orWhere($column, $operator, $value);
+    }
+
+    public function scopeOrLike($query, $column, $value, $side = 'both', $isNotLike = false)
+    {
+        return $query->like($column, $value, $side, $isNotLike, false);
+    }
+
+    public function scopeNotLike($query, $column, $value, $side = 'both', $isAnd = true)
+    {
+        return $query->like($column, $value, $side, true, $isAnd);
+    }
+
+    public function scopeOrNotLike($query, $column, $value, $side = 'both')
+    {
+        return $query->like($column, $value, $side, true, false);
     }
 
 }
